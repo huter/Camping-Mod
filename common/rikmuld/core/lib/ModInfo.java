03b6215e4fe7b0fafa034c947129ef5002380ce0
Rikmuld <rikmuld@gmail.com>
2013-03-25 21:36:36 +0100

    Updated version system to new build system

--- common/rikmuld/core/lib/ModInfo.java
+++ common/rikmuld/core/lib/ModInfo.java
@@ -4,7 +4,7 @@ public class ModInfo {
 
 	public static final String MOD_ID = "camping";
 	public static final String MOD_NAME = "The Camping Mod";
-	public static final String MOD_VERSION = "1.0.1a";
+	public static final String MOD_VERSION = "VERSION";
 	public static final String MOD_DEPENDENCIES = "required-after:Forge@[v7.7.0.600,)";
 	public static final String MOD_SERVER_PROXY = "rikmuld.core.proxys.CommonProxy";
 	public static final String MOD_CLIENT_PROXY = "rikmuld.core.proxys.ClientProxy";
